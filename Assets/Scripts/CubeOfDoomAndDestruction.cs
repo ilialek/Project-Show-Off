5b98a5f7adb82f9020036155e328daaae54ead02
Seventyy <seventyyyyy@gmail.com>
2024-05-22 16:03:01 +0200

    EventBus first iteration with placeholders

---
+++ Assets/Scripts/CubeOfDoomAndDestruction.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeOfDoomAndDestruction : MonoBehaviour, IEventListener
+{
+    void Start()
+    {
+        EventBus.Instance.Register(this);
+    }
+
+    public void OnEvent(Event e)
+    {
+        if (e is EventLanternGoneOut)
+        {
+            DoDoomAndDeathAndStuff();
+        }
+    }
+
+    void DoDoomAndDeathAndStuff()
+    {
+        Debug.Log("Doom and destruction!");
+        transform.position = new Vector3(transform.position.x, 0.5f, transform.position.z);
+    }
+
+    void UndoDoom()
+    {
+        Debug.Log("Undoing doom (or undooming for short)");
+        transform.position = new Vector3(transform.position.x, 2f, transform.position.z);
+    }
+
+
+}
